9e2d546c640ebcd7b3aae87ffe8373646958a052
Samin Israr Ravi <saminisrar1@gmail.com>
2025-12-01 15:13:14 +0600

    cors

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.post(
 );
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', "https://deploy-frontend-ph-healthcare.vercel.app"],
     credentials: true
 }));
 
